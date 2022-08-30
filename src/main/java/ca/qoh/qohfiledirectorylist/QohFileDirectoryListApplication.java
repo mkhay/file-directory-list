d30654761094d377f27983780698e1bf052b0d2f
mkhay <mohamed.khayata@gmail.com>
2022-08-30 14:47:48 -0400

    Initial basic spring boot skeleton

---
+++ src/main/java/ca/qoh/qohfiledirectorylist/QohFileDirectoryListApplication.java
@@ -0,0 +1,13 @@
+package ca.qoh.qohfiledirectorylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QohFileDirectoryListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QohFileDirectoryListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/qoh/qohfiledirectorylist/QohFileDirectoryListApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.qoh.qohfiledirectorylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QohFileDirectoryListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
